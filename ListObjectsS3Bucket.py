ef4d1b637aa9179f2d567441831447039db2b1c7
Hoova T <htapplied@yahoo.com>
2023-04-11 16:51:19 +0000

    List Objects in S3 Bucket

---
+++ ListObjectsS3Bucket.py
@@ -0,0 +1,13 @@
+import boto3
+
+s3_resource=boto3.client("s3")
+
+objects=s3_resource.list_objects(Bucket="hoov-test-bucket")["Contents"]
+
+len(objects)
+
+if len(objects)>0:
+    print ("objects exists")
+
+for object in objects:
+    print (object)

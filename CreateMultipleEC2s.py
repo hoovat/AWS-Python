9bd3766f93d33579c48527ff1eb38c8e1d50fbdd
Hoova T <htapplied@yahoo.com>
2023-04-13 15:50:33 +0000

    Create Multiple EC2 Instances

---
+++ CreateMultipleEC2s.py
@@ -0,0 +1,8 @@
+import boto3
+ec2_resource=boto3.resource("ec2")
+ec2_resource.create_instances(ImageId='ami-06e46074ae430fba6',
+InstanceType='t1.micro',
+    MaxCount=3,
+    MinCount=3,)
+    
+print("New EC2 Instances Created")

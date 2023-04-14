2f00276b2b5fe5483f8b34ea32c1faaf9cb7943e
Hoova T <123920433+hoovat@users.noreply.github.com>
2023-04-14 15:27:40 -0400

    Update CreateMultipleEC2s.py

--- CreateMultipleEC2s.py
+++ CreateMultipleEC2s.py
@@ -1,7 +1,7 @@
 import boto3
 ec2_resource=boto3.resource("ec2")
 ec2_resource.create_instances(ImageId='ami-06e46074ae430fba6',
-InstanceType='t1.micro',
+InstanceType='t2.micro',
     MaxCount=3,
     MinCount=3,)
     
